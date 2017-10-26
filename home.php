e9524e7229e6a63d6112f88612bccdaeb11e52db
roy0111 <biratewu0111@gmail.com>
2017-10-26 20:57:00 +0600

    Add files via upload

---
+++ home.php
@@ -0,0 +1,583 @@
+
+
+<!DOCTYPE html>
+<!--[if IE 8 ]><html class="no-js oldie ie8" lang="en"> <![endif]-->
+<!--[if IE 9 ]><html class="no-js oldie ie9" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+
+   <!--- basic page needs
+   ================================================== -->
+   <meta charset="utf-8">
+	<title>My Diary</title>
+	<meta name="description" content="">  
+	<meta name="author" content="">
+
+   <!-- mobile specific metas
+   ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+ 	<!-- CSS
+   ================================================== -->
+   <link rel="stylesheet" href="css/base.css">
+   <link rel="stylesheet" href="css/vendor.css">  
+   <link rel="stylesheet" href="css/main.css">  
+
+   <!-- script
+   ================================================== -->
+	<script src="js/modernizr.js"></script>
+	<script src="js/pace.min.js"></script>
+
+   <!-- favicons
+	================================================== -->
+	<link rel="shortcut icon" href="iconMD.png" type="image/x-icon">
+	<link rel="icon" href="iconMD.png" type="image/x-icon">
+
+</head>
+
+<body id="top">
+
+	<!-- header 
+   ================================================== -->
+   <header> 
+
+ 
+
+		<a id="header-menu-trigger" href="#0">
+		 	<span class="header-menu-text">Menu</span>
+		  	<span class="header-menu-icon"></span>
+		</a> 
+
+		<nav id="menu-nav-wrap">
+
+			<a href="#0" class="close-button" title="close"><span>Close</span></a>	
+
+	   	<h3>My Diary.</h3>  
+
+			<ul class="nav-list">
+				<li class="current"><a class="smoothscroll" href="#home" title="">Home</a></li>
+				<li><a class="smoothscroll" href="#about" title="">About</a></li>
+				<li><a class="smoothscroll" href="#services" title="">Services</a></li>
+				<li><a class="smoothscroll" href="#portfolio" title="">Works</a></li>
+				<li><a class="smoothscroll" href="#contact" title="">Contact</a></li>						
+			</ul>	
+		
+
+		<a href="login_page.php" target="_blank">
+           
+         <button type="button" class="btn btn-success" style="color: #ffffff; background-color: MediumSeaGreen">LOG IN</button>
+          </a>
+       
+	      </ul>		
+
+		</nav>  <!-- end #menu-nav-wrap -->
+
+	</header> <!-- end header -->  
+
+
+   <!-- home
+   ================================================== -->
+   <section id="home">
+
+   	<div class="overlay"></div>
+
+   	<div class="home-content-table">	
+		   <div class="home-content-tablecell">
+		   	<div class="row">
+		   		<div class="col-twelve">		   			
+			  		
+			  				<h3 class="animate-intro">Explore Everyday</h3>
+				  			<h1 class="animate-intro">
+							Smile at the past.<br>
+							Run towards to future.
+				  			</h1>	
+
+				  			<div class="more animate-intro">
+				  				<a class="smoothscroll button stroke" href="#about">
+				  					Learn More
+				  				</a>
+		                     
+		                     <a href="login_page.php" target="_blank">
+           
+                                <button type="button" class="btn btn-success" style="color: #ffffff; background-color: MediumSeaGreen">LOG IN</button>
+                               </a>
+				  			</div>							
+
+			  		</div> <!-- end col-twelve --> 
+		   	</div> <!-- end row --> 
+		   </div> <!-- end home-content-tablecell --> 		   
+		</div> <!-- end home-content-table -->
+
+		<ul class="home-social-list">
+	      <li class="animate-intro">
+	        	<a href="https://github.com/roy0111" target="_blank"><i class="fa fa-github "></i></a>
+	      </li>
+<!-- 	      <li class="animate-intro">
+	        	<a href="#"><i class="fa fa-twitter"></i></a>
+	      </li>
+	      <li class="animate-intro">
+	        	<a href="#"><i class="fa fa-instagram"></i></a>
+	      </li>
+       -->
+	   </ul> <!-- end home-social-list -->	
+
+		<div class="scrolldown">
+			<a href="#about" class="scroll-icon smoothscroll">		
+		   	Scroll Down		   	
+		   	<i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+			</a>
+		</div>			
+   
+   </section> <!-- end home -->
+
+
+   <!-- about
+   ================================================== -->
+   <section id="about">
+
+   	<div class="row about-wrap">
+   		<div class="col-full">
+
+   			<div class="about-profile-bg"></div>
+
+				<div class="intro">
+					<h3 class="animate-this">About</h3>
+	   			<p class="lead animate-this"><span>My Diary</span> is a creative digital online e-diary. Here you can bind your everyday memories, experiences,
+	   			event plan, secure up your important imformation etc.Everyone can have their own personal online diary - it's free at My Diary!  Go ahead and create your own private diary today. </p>	
+				</div>   
+
+   		</div> <!-- end col-full  -->
+   	</div> <!-- end about-wrap  -->
+
+   </section> <!-- end about -->
+
+
+   <!-- about
+   ================================================== -->
+   <section id="services">
+
+   	<div class="overlay"></div>
+   	<div class="gradient-overlay"></div>
+   	
+   	<div class="row narrow section-intro with-bottom-sep animate-this">
+   		<div class="col-full">
+   			
+   				<h3>Services</h3>
+   			   <h1>What You Can Do.</h1>
+   			
+   			   <p class="lead">Whether you’re looking for a tool to record your daily emotions and activities in areflective journal, keep track of milestones in afood diary orpregnancy journal, or even record your dreams in a dream journal, My Diary has you covered.My Diary gives you all the tools you need to focus on the ideas you want to preserve, rather than the process of writing itself.</p>
+   			
+   	   </div> <!-- end col-full -->
+   	</div> <!-- end row -->
+
+   	<div class="row services-content">
+
+   		<div class="services-list block-1-2 block-tab-full group">
+
+	      	<div class="bgrid service-item animate-this">	
+
+	      		<span class="icon"><i class=" fa fa-lock"></i></span>            
+
+	            <div class="service-content">
+	            	<h3 class="h05">100% secure</h3>
+
+		            <p>Once you save a password in My Diary, you'll always have it when you need it; logging in is fast and easy. My Diary is a password manager and password generator that locks your passwords and personal information in a secure vault. My Diary autofills web browser and app logins for you, and generates new, secure passwords instantly.<br>
+		            Private notes – Secure safe info like credit cards, photos,health insurance, credit card info & more safe
+
+	         		</p>
+
+	         	</div> 	         	 
+
+				</div> <!-- end bgrid -->
+
+				<div class="bgrid service-item animate-this">	
+
+					<span class="icon"><i class="fa fa-shield"></i></span>                          
+
+	            <div class="service-content">	
+	            	<h3 class="h05">Emergency Access</h3>  
+
+		            <p>- Plan ahead and give trusted family or friends a way to access your vault in case of an emergency.Add security and require a waiting period during which you can decline an Emergency Access request to your vault.Once access is granted, your vault syncs as a folder to their vault.
+	         		</p>	         		
+	            </div>	                          
+
+			   </div> <!-- end bgrid -->
+
+			   <div class="bgrid service-item animate-this">
+
+			   	<span class="icon"><i class="fa fa-sticky-note-o"></i></span>		            
+
+	            <div class="service-content">
+	            	<h3 class="h05">Save Note</h3>
+
+		            <p>Organize your  plans in your own digital notebook. Keep everything in a My Diary, and use its powerful search to find what you're looking for, even text in pictures or htypewriting.Your notebooks are saved automatically and can be viewed from any devices, online or offline.
+	        			</p>
+	            </div> 	            	               
+
+			   </div> <!-- end bgrid -->
+
+				<div class="bgrid service-item animate-this">
+
+					<span class="icon"><i class="fa fa-bell"></i></span>	              
+
+	            <div class="service-content">
+	            	<h3 class="h05">Event Plan</h3>
+
+		            <p>While this is a high-level overview of some of the steps you’ll need to follow when developing an event, we can't identify all of the details you’ll need to think about (since much of it will depend on the type of event you are holding). But, we hope the following provides a starting point for your event planning.It notify you by its own.
+	         		</p>	         		
+	            </div>                
+
+				</div> <!-- end bgrid -->	
+
+
+
+
+	      </div> <!-- end services-list -->
+   		
+   	</div> <!-- end services-content -->   			
+
+   </section> <!-- end services -->
+
+
+   <!-- portfolio
+   ================================================== -->
+   <section id="portfolio">
+   	
+   	<div class="intro-wrap">
+
+   		<div class="row narrow section-intro with-bottom-sep animate-this">
+	   		<div class="col-twelve">
+	   			<h3>Showcase</h3>
+		   		<h1>Some Feature</h1>  			
+		   		
+		   	   			
+	   		</div>   		
+	   	</div> <!-- end row section-intro -->   		
+
+   	</div> <!-- end intro-wrap -->   	
+
+   	<div class="row portfolio-content">
+   		<div class="col-twelve">
+   			<div id="folio-wrap" class="bricks-wrapper">					
+
+   				<div class="brick folio-item">
+	               <div class="item-wrap animate-this" data-src="images/portfolio/screenshot/topView.png" data-sub-html="#01" > 	
+	                  <a href="#" class="overlay">
+	                  <img src="images/portfolio/screenshot/topView.png" alt="Skaterboy">	                     
+	                     <div class="item-text">
+	                     	<span class="folio-types">
+		     					      Events and photo
+		     					   </span>
+		     					   <h3 class="folio-title"></h3>	     					   
+		     					</div>                                        
+	                  </a>
+	                 
+	               </div> <!-- end item-wrap -->
+						
+						<div id="05" class='hide'>
+							<h4>Start up</h4>
+							
+						</div>
+	        		</div> <!-- end folio-item -->
+
+	        	
+	        	   	<div class="brick folio-item">
+	               <div class="item-wrap animate-this" data-src="images/portfolio/screenshot/login.png" data-sub-html="#05" > 	
+	                  <a href="#" class="overlay">
+	                  	<img src="images/portfolio/screenshot/login.png" alt="Skaterboy">	                     
+	                     <div class="item-text">
+	                     	<span class="folio-types">
+		     					      Events and photo
+		     					   </span>
+		     					   <h3 class="folio-title"></h3>	     					   
+		     					</div>                                        
+	                  </a>
+	                 
+	               </div> <!-- end item-wrap -->
+						
+						<div id="05" class='hide'>
+							<h4>Start up</h4>
+							
+						</div>
+	        		</div> <!-- end folio-item -->
+
+	        	<div class="brick folio-item">
+	               <div class="item-wrap animate-this" data-src="images/portfolio/screenshot/lockerAddView.png" data-sub-html="#02"> 	
+	                  <a href="#" class="overlay">
+	                  	<img src="images/portfolio/screenshot/lockerAddView.png" alt="Shutterbug">	                     
+	                     <div class="item-text">
+	                     	<span class="folio-types">
+		     					      Last Pass View
+		     					   </span>
+		     					   <h3 class="folio-title">View</h3>  	     					   
+		     					</div>                                        
+	                  </a>
+	                 
+	               </div> <!-- end item-wrap -->
+
+	    
+	                           
+	        		</div> <!-- end folio-item -->
+
+
+
+				<div class="brick folio-item">
+	               <div class="item-wrap animate-this"  data-src="images/portfolio/screenshot/register.png"  data-sub-html="#06" >  	
+	                  <a href="#" class="overlay">
+	                  	<img src="images/portfolio/screenshot/register.png" alt="Bicycle">	                     
+	                     <div class="item-text">	     					    
+		     					   <span class="folio-types">
+		     					      Registration Page
+		     					   </span>
+		     					   <h3 class="folio-title">Registration</h3>
+		     					</div>                                        
+	                  </a>
+	        
+	               </div> <!-- end item-wrap -->
+
+	               <div id="06" class='hide'>
+							<h4>Registration</h4>
+						 
+						</div>	
+					
+
+					<div class="item-wrap animate-this"  data-src="images/portfolio/screenshot/friendDetails.png"  data-sub-html="#06" >  	
+	                  <a href="#" class="overlay">
+	                  	<img src="images/portfolio/screenshot/friendDetails.png" alt="Bicycle">	                     
+	                     <div class="item-text">	     					    
+		     					   <span class="folio-types">
+		     					      Contacts
+		     					   </span>
+		     					   <h3 class="folio-title">Contact details</h3>
+		     					</div>                                        
+	                  </a>
+	        
+	               </div> <!-- end item-wrap -->
+
+	               <div id="06" class='hide'>
+							<h4>Contact details</h4>
+							
+						</div>	
+
+
+
+
+	
+
+
+	        		</div> <!-- end folio-item -->
+
+
+
+				<div class="brick folio-item">
+	               <div class="item-wrap animate-this"  data-src="images/portfolio/screenshot/eventView.png"  data-sub-html="#05" >  	
+	                  <a href="#" class="overlay">
+	                  	<img src="images/portfolio/screenshot/eventView.png" alt="Bicycle">	                     
+	                     <div class="item-text">	     					    
+		     					   <span class="folio-types">
+		     					      Event
+		     					   </span>
+		     					   <h3 class="folio-title">Event xplore</h3>
+		     					</div>                                        
+	                  </a>
+	        
+	               </div> <!-- end item-wrap -->
+
+	               <div id="05" class='hide'>
+							<h4>Event</h4>
+						 
+						</div>	
+					
+
+                
+	        		</div> <!-- end folio-item -->
+	           
+	  				
+
+   			</div> <!-- end folio-wrap -->
+   		</div> <!-- end twelve -->
+   	</div> <!-- end portfolio-content -->   	
+
+   </section>  <!-- end portfolio -->
+
+
+   <!-- Testimonials Section
+   ================================================== -->
+   <section id="testimonials">
+
+   	<div class="row">
+   		<div class="col-twelve">
+   			<h2 class="animate-this">Motivational Speach.</h2>
+   		</div>   		
+   	</div>   	
+
+      <div class="row flex-container">
+    
+         <div id="testimonial-slider" class="flex-slider animate-this">
+
+            <ul class="slides">
+
+               <li>
+                   <i>
+                  The 3 C's of life: 
+                  <i>CHOICE, CHANCE,CHANGE.</i>
+                  You must make a choice to make a chance or your life will never change.   
+                  </i>
+
+                  <div class="testimonial-author">
+                    	
+                    	<div class="author-info">
+                    		<span>Unknown</span>
+                    	</div>
+                  </div>                 
+             	</li> <!-- end slide -->
+
+               <li>
+                  <i>
+                  Planning foe Success will make you even more creative.   
+                  </i>
+
+               	<div class="testimonial-author">
+                    	
+                    	<div class="author-info">
+                    		<span>Danielle Laporte & Linda Sivertsen</span>
+             
+                    	</div>
+                  </div>                                         
+               </li> <!-- end slide -->
+
+            </ul> <!-- end slides -->
+
+         </div> <!-- end testimonial-slider -->         
+        
+      </div> <!-- end flex-container -->
+
+   </section> <!-- end testimonials -->
+
+
+	<!-- stats
+   ================================================== -->
+   <section id="clients">
+      
+		<div class="row animate-this">
+			<div class="col-twelve">
+			<div> <d1>Used Tools, Languages</d1></div>
+
+				<div class="client-lists owl-carousel">
+  					<div><img src="images/clients/mozilla.png" alt=""></div>
+  					<div><img src="images/clients/firefox.png" alt=""></div>
+  					<div><img src="images/clients/jQuery.png" alt=""></div>
+  					<div><img src="images/clients/github.png" alt=""></div>
+   					<div><img src="images/clients/codepen.png" alt=""></div> 
+   					<div><img src="images/clients/php.png" alt=""></div>	
+   					<div><img src="images/clients/angular.png" alt=""></div>
+   					<div><img src="images/clients/c.png" alt=""></div>
+   					<div><img src="images/clients/index.jpeg" alt=""></div>			
+				</div>
+				
+			</div> <!-- end col-twelve -->
+		</div> <!-- end row -->
+
+   </section> <!-- end clients -->
+
+
+	<!-- contact
+   ================================================== -->
+   <section id="contact">
+
+    
+
+   	<div class="row contact-content">
+
+         <div class="col-four tab-full contact-info end animate-this">
+
+         	<h5>Contact Information</h5>
+
+	   		<div class="cinfo">
+	   			<h6>Birat saha</h6>
+	   			<p>
+	   				Mail: biratewu0111@gmail.com<br>
+				   	East West University<br>
+	            	1212-Dhaka, Bnagladesh<br>
+	            	Phone: 016*******<br>
+				   	Mobile: (+63) 21545454***<br>	     
+				   </p>
+	   		</div> <!-- end cinfo -->
+         	
+	   		<div class="cinfo">
+	   			<h6>Md. Tasdid</h6>
+	   			<p>
+	   				Mail: tasdid@gmail.com<br>
+				   	East West University<br>
+	            	1212-Dhaka, Bnagladesh<br>
+	            	Phone: 01********<br>
+				   	Mobile: (+63) 21565454***<br>	     
+				   </p>
+	   		</div> <!-- end cinfo -->
+
+	   	  <div class="cinfo">
+	   			<h6>Kanij</h6>
+	   			<p>
+	   				Mail: kanij@gmail.com<br>
+				   	East West University<br>
+	            	1212-Dhaka, Bnagladesh<br>
+	            	Phone: 01********<br>
+				   	Mobile: (+63) 21545454***<br>	     
+				   </p>
+	   		</div> <!-- end cinfo -->
+         </div> <!-- end cinfo --> 
+
+
+
+     	  <div class="col-four tab-full contact-info end animate-this">     
+	      		<h5 >Reference</h5>
+	      		<ul style="color:rgb(255, 0, 0)">
+
+	      			<li ><a href="https://www.lastpass.com/"><span style="color:MediumSeaGreen">LastPass</span></a></li>
+
+						<li ><a href="https://www.styleshout.com/"><span style="color:MediumSeaGreen">Styleshout</span></a></li>
+
+						<li><a href="https://unsplash.com/"><span style="color:MediumSeaGreen">Unplash</span></a></li>
+
+						<li><a href="https://www.w3schools.com"><span style="color:MediumSeaGreen">W3School</span></a></li>
+											
+					</ul>
+
+	      	</div> 
+
+
+   	</div> <!-- end row contact-content -->
+		
+	</section> <!-- end contact -->
+
+
+	
+   
+
+<footer>
+
+
+      <div id="go-top">
+		   <a class="smoothscroll" title="Back to Top" href="#top">
+		   	<i class="fa fa-long-arrow-up" aria-hidden="true"></i>
+		   </a>
+		</div>		
+
+   </footer>
+
+   <div id="preloader"> 
+    	<div id="loader"></div>
+   </div> 
+
+	      		<div style="align-self: center; background-color: #b4b4b4;text-align: center;" >
+		         	 
+		         		         	
+		         </div>	
+<!-- ? +++++++++++++++++++++++++++++++++++++++++++++++++++++++++==-->
+   <script src="js/jquery-2.1.3.min.js"></script>
+   <script src="js/plugins.js"></script>
+   <script src="js/main.js"></script>
+
+</body>
+
+</html>
